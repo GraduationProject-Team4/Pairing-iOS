4161edd92a69aef8b2e822e3d74b4c99a72c5d0f
youz2me <kynhun20@gachon.ac.kr>
2023-08-02 16:42:26 +0900

    Build: feature/10-enviormentSoundAnalyze 브랜치 생성 및 AnalysisResultView 생성
    #10

---
+++ Pairing/Sources/Views/EnvironmentAnalysis/AnalysisResultView.swift
@@ -0,0 +1,20 @@
+//
+//  AnalysisResultView.swift
+//  Pairing
+//
+//  Created by YOUJIM on 2023/08/02.
+//
+
+import SwiftUI
+
+struct AnalysisResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnalysisResultView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnalysisResultView()
+    }
+}
